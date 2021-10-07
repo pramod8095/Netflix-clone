11aba18e4d6b8c24aead4f7bb126d08f2255de77
pramod <kpramod8095@allstate.com>
2021-10-08 05:07:23 +0530

    commit

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -1,9 +1,43 @@
-import React from 'react'
-
+import React,{ useState, useEffect } from 'react'
+import axios from '../Axios/axios'
+import requests from '../Request/request'
+import './Banner.css'
 export default function Banner() {
+    const [movie, setMovie] = useState([])
+
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(requests.fetchHorrorMovies)
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length -1)
+                ]
+            )
+        }
+        fetchData();
+    }, [])
+
     return (
-        <header>
-            
+        <header className='banner'
+        style={{
+            backgroundSize:'cover',
+            backgroundImage:`url(
+                "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+            )`,
+            backgroundPosition:"center center"
+        }}>
+            <div className='banner_contents'>
+                <h1 className='banner_title'>
+                    {movie?.title}
+                </h1>
+                <div className='banner_buttons'>
+                    <button className='banner_button'>Play</button>
+                    <button className='banner_button'>+My List</button>
+                </div>
+                <h1 className='banner_description'>{movie?.overview}</h1>
+
+            </div>
+
             
         </header>
     )
