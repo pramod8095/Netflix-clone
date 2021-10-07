c5c9e318daaa5e43d8c4a2f195c2b7ddfdcbea17
pramod <kpramod8095@allstate.com>
2021-10-08 04:12:50 +0530

    netflix

---
+++ src/Table/Row.js
@@ -0,0 +1,33 @@
+import React, { useState,useEffect } from 'react'
+import axios from '../Axios/axios';
+import requests from '../Request/request';
+import './Row.css'
+
+const baseUrl = "https://image.tmdb.org/t/p/original/"
+
+export default function Row({title,fetchUrl, isLargeRow}) {
+    const [movies, setMovies] = useState([]);
+    useEffect(()=>{
+        async function fetchData(){
+            const request = await axios.get(fetchUrl)
+            setMovies(request.data.results)
+            return request
+        }
+        fetchData();
+    },[fetchUrl])
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+
+            <div className={`row__posters`}>
+                {movies.map(movie => (
+                    <img 
+                    key={movie.id}
+                    className={`row_poster ${isLargeRow && 'row_posterLarge'}`}
+                    src={`${baseUrl}${isLargeRow ? movie.poster_path:movie.backdrop_path}`} alt={movie.name}/>
+                ))}
+
+            </div>
+        </div>
+    )
+}
